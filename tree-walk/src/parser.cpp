3166ce993c7b27f03a27a33cb73f3a47eb9ed574
pmarshall111 <pmarshall.dev@gmail.com>
2025-05-10 15:13:42 +0100

    Parse multiple statements

--- tree-walk/src/parser.cpp
+++ tree-walk/src/parser.cpp
@@ -1,5 +1,13 @@
 #include <parser.h>
 
+// program        → statement* EOF ;
+//
+// statement      → exprStmt
+//                | printStmt ;
+//
+// exprStmt       → expression ";" ;
+// printStmt      → "print" expression ";" ;
+
 // expression     → equality ;
 // equality       → comparison ( ( "!=" | "==" ) comparison )* ;
 // comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
@@ -17,11 +25,31 @@ namespace treewalk {
 
 namespace {
 
-std::unique_ptr<ast::Expr> expression(const std::vector<Token> &tokens,
-                                      int &pos, std::vector<ParseError> &errs);
+// A wrapper around ParseError. Using exceptions helps to stop parsing a bad
+// statement.
+// TODO: Replace Errors with just these exception classes?
+class ParseException : public std::exception {
+public:
+  explicit ParseException(const ParseError &err) : d_err(err) {}
 
-std::unique_ptr<ast::Expr> primary(const std::vector<Token> &tokens, int &pos,
-                                   std::vector<ParseError> &errs) {
+  const char *what() const noexcept override {
+    static std::string str;
+    std::ostringstream ss;
+    ss << d_err;
+    str = ss.str();
+    return str.c_str();
+  }
+
+  const ParseError &getErr() const noexcept { return d_err; }
+
+private:
+  ParseError d_err;
+};
+
+std::unique_ptr<ast::Expr> expression(const std::vector<Token> &tokens,
+                                      int &pos);
+
+std::unique_ptr<ast::Expr> primary(const std::vector<Token> &tokens, int &pos) {
   switch (tokens[pos].type) {
   case TokenType::NUMBER:
   case TokenType::STRING:
@@ -38,53 +66,47 @@ std::unique_ptr<ast::Expr> primary(const std::vector<Token> &tokens, int &pos,
       pos++;
       return grp;
     }
-    errs.push_back({"No closing paren found!"});
-    return std::unique_ptr<ast::Expr>();
+    throw ParseException({"No closing paren found!"});
   }
   default:
-    errs.push_back({"Unknown token!"});
-    return std::unique_ptr<ast::Expr>();
+    throw ParseException({"Unknown token!"});
   }
 }
 
-std::unique_ptr<ast::Expr> unary(const std::vector<Token> &tokens, int &pos,
-                                 std::vector<ParseError> &errs) {
+std::unique_ptr<ast::Expr> unary(const std::vector<Token> &tokens, int &pos) {
   switch (tokens[pos].type) {
   case TokenType::BANG:
   case TokenType::MINUS: {
     const Token &tok = tokens[pos];
-    return std::make_unique<ast::Expr>(
-        ast::Unary{tok, unary(tokens, ++pos, errs)});
+    return std::make_unique<ast::Expr>(ast::Unary{tok, unary(tokens, ++pos)});
   }
   default:
-    return primary(tokens, pos, errs);
+    return primary(tokens, pos);
   }
 }
 
-std::unique_ptr<ast::Expr> factor(const std::vector<Token> &tokens, int &pos,
-                                  std::vector<ParseError> &errs) {
-  std::unique_ptr<ast::Expr> leftOp = unary(tokens, pos, errs);
+std::unique_ptr<ast::Expr> factor(const std::vector<Token> &tokens, int &pos) {
+  std::unique_ptr<ast::Expr> leftOp = unary(tokens, pos);
   switch (tokens[pos].type) {
   case TokenType::STAR:
   case TokenType::SLASH: {
     const Token &tok = tokens[pos];
     return std::make_unique<ast::Expr>(
-        ast::Binary{std::move(leftOp), tok, factor(tokens, ++pos, errs)});
+        ast::Binary{std::move(leftOp), tok, factor(tokens, ++pos)});
   }
   default:
     return leftOp;
   }
 }
 
-std::unique_ptr<ast::Expr> term(const std::vector<Token> &tokens, int &pos,
-                                std::vector<ParseError> &errs) {
-  std::unique_ptr<ast::Expr> leftOp = factor(tokens, pos, errs);
+std::unique_ptr<ast::Expr> term(const std::vector<Token> &tokens, int &pos) {
+  std::unique_ptr<ast::Expr> leftOp = factor(tokens, pos);
   switch (tokens[pos].type) {
   case TokenType::PLUS:
   case TokenType::MINUS: {
     const Token &tok = tokens[pos];
     return std::make_unique<ast::Expr>(
-        ast::Binary{std::move(leftOp), tok, term(tokens, ++pos, errs)});
+        ast::Binary{std::move(leftOp), tok, term(tokens, ++pos)});
   }
   default:
     return leftOp;
@@ -92,8 +114,8 @@ std::unique_ptr<ast::Expr> term(const std::vector<Token> &tokens, int &pos,
 }
 
 std::unique_ptr<ast::Expr> comparison(const std::vector<Token> &tokens,
-                                      int &pos, std::vector<ParseError> &errs) {
-  std::unique_ptr<ast::Expr> leftOp = term(tokens, pos, errs);
+                                      int &pos) {
+  std::unique_ptr<ast::Expr> leftOp = term(tokens, pos);
   switch (tokens[pos].type) {
   case TokenType::LESS:
   case TokenType::LESS_EQUAL:
@@ -101,22 +123,22 @@ std::unique_ptr<ast::Expr> comparison(const std::vector<Token> &tokens,
   case TokenType::GREATER_EQUAL: {
     const Token &tok = tokens[pos];
     return std::make_unique<ast::Expr>(
-        ast::Binary{std::move(leftOp), tok, comparison(tokens, ++pos, errs)});
+        ast::Binary{std::move(leftOp), tok, comparison(tokens, ++pos)});
   }
   default:
     return leftOp;
   }
 }
 
-std::unique_ptr<ast::Expr> equality(const std::vector<Token> &tokens, int &pos,
-                                    std::vector<ParseError> &errs) {
-  std::unique_ptr<ast::Expr> leftOp = comparison(tokens, pos, errs);
+std::unique_ptr<ast::Expr> equality(const std::vector<Token> &tokens,
+                                    int &pos) {
+  std::unique_ptr<ast::Expr> leftOp = comparison(tokens, pos);
   switch (tokens[pos].type) {
   case TokenType::BANG_EQUAL:
   case TokenType::EQUAL_EQUAL: {
     const Token &tok = tokens[pos];
     return std::make_unique<ast::Expr>(
-        ast::Binary{std::move(leftOp), tok, equality(tokens, ++pos, errs)});
+        ast::Binary{std::move(leftOp), tok, equality(tokens, ++pos)});
   }
   default:
     return leftOp;
@@ -124,16 +146,77 @@ std::unique_ptr<ast::Expr> equality(const std::vector<Token> &tokens, int &pos,
 }
 
 std::unique_ptr<ast::Expr> expression(const std::vector<Token> &tokens,
-                                      int &pos, std::vector<ParseError> &errs) {
-  return equality(tokens, pos, errs);
+                                      int &pos) {
+  return equality(tokens, pos);
+}
+
+std::unique_ptr<stmt::Stmt> printStatement(const std::vector<Token> &tokens,
+                                           int &pos) {
+  std::unique_ptr<ast::Expr> expr = expression(tokens, pos);
+  switch (tokens[pos].type) {
+  case TokenType::SEMICOLON: {
+    pos++;
+    return std::make_unique<stmt::Stmt>(stmt::Print{std::move(expr)});
+  }
+  default:
+    // TODO: make better error message - line? surrounding toks?
+    throw ParseException({"No ending semi colon found!"});
+  }
+}
+
+std::unique_ptr<stmt::Stmt> exprStatement(const std::vector<Token> &tokens,
+                                          int &pos) {
+  std::unique_ptr<ast::Expr> expr = expression(tokens, pos);
+  switch (tokens[pos].type) {
+  case TokenType::SEMICOLON: {
+    pos++;
+    return std::make_unique<stmt::Stmt>(stmt::Expression{std::move(expr)});
+  }
+  default:
+    // TODO: make better error message - line? surrounding toks?
+    throw ParseException({"No ending semi colon found!"});
+  }
+}
+
+std::unique_ptr<stmt::Stmt> statement(const std::vector<Token> &tokens,
+                                      int &pos) {
+  switch (tokens[pos].type) {
+  case TokenType::PRINT:
+    return printStatement(tokens, ++pos);
+  default:
+    return exprStatement(tokens, pos);
+  }
+}
+
+void skipUntilSemiColon(const std::vector<Token> &tokens, int &pos) {
+  while (tokens[pos].type != TokenType::SEMICOLON && pos < tokens.size()) {
+    pos++;
+  }
+  if (pos < tokens.size()) {
+    // Found a semi-colon. Continue to next char
+    pos++;
+  }
 }
 
 } // namespace
 
-std::unique_ptr<ast::Expr> parse(const std::vector<Token> &tokens,
-                                 std::vector<ParseError> &errs) {
+std::vector<stmt::Stmt> parse(const std::vector<Token> &tokens,
+                              std::vector<ParseError> &errs) {
+  std::vector<stmt::Stmt> statements;
   int pos = 0;
-  return expression(tokens, pos, errs);
+  while (pos != tokens.size()) {
+    try {
+      auto s = statement(tokens, pos);
+      // TODO: Check statement is not null
+      statements.push_back(std::move(*s));
+    } catch (const ParseException &e) {
+      // Error while parsing this statement. Continue parsing the next statement
+      // so the user knows all errors in their code.
+      skipUntilSemiColon(tokens, pos);
+      errs.push_back(e.getErr());
+    }
+  }
+  return statements;
 }
 
 } // namespace treewalk
