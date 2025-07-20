9632db76cf4db24f763fd3ffcb3e7826a0330d25
pmarshall111 <pmarshall.dev@gmail.com>
2025-07-20 18:36:26 +0100

    Add system test for a function with arguments

--- tree-walk/systemtest/test_fun.py
+++ tree-walk/systemtest/test_fun.py
@@ -11,3 +11,20 @@ def test_native_fun(lox_runner):
     # THEN
     assert stdout.strip().splitlines() == ["tree-walk"]
     assert stderr == ""
+
+
+def test_user_fun_args(lox_runner):
+    # GIVEN
+    code = """
+    fun printWithSpace(arg1, arg2) {
+        print arg1 + " " + arg2;
+    }
+    printWithSpace("hello", "world");
+    """
+
+    # WHEN
+    stdout, stderr = lox_runner(code)
+
+    # THEN
+    assert stdout.strip().splitlines() == ["hello world"]
+    assert stderr == ""
