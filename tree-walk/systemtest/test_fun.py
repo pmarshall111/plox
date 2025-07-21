32f48058341a293663fd5a7df648187e12133560
pmarshall111 <pmarshall.dev@gmail.com>
2025-07-21 18:04:46 +0100

    Add tests for the number of args in a function

--- tree-walk/systemtest/test_fun.py
+++ tree-walk/systemtest/test_fun.py
@@ -28,3 +28,37 @@ def test_user_fun_args(lox_runner):
     # THEN
     assert stdout.strip().splitlines() == ["hello world"]
     assert stderr == ""
+
+
+def test_fun_fewer_args(lox_runner):
+    # GIVEN
+    code = """
+    fun oneArg(arg1) {
+    }
+    oneArg();
+    """
+
+    # WHEN
+    stdout, stderr = lox_runner(code)
+
+    # THEN
+    assert stdout.strip().splitlines() == []
+    assert "accepts 1 args" in stderr.strip()
+
+
+def test_fun_more_args(lox_runner):
+    # GIVEN
+    code = """
+    fun oneArg(arg1) {
+    }
+    oneArg("a", "b", "c");
+    """
+
+    # WHEN
+    stdout, stderr = lox_runner(code)
+
+    # THEN
+    assert stdout.strip().splitlines() == []
+    assert "accepts 1 args" in stderr.strip()
+
+
