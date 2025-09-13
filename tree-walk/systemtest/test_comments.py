820e9e6442c5645ca531dab166a91490ce5e0c11
pmarshall111 <pmarshall.dev@gmail.com>
2025-09-13 17:04:25 +0100

    Add comments

---
+++ tree-walk/systemtest/test_comments.py
@@ -0,0 +1,15 @@
+def test_comments(lox_runner):
+    # GIVEN
+    code = """
+    var a = 15; // This is a variable a
+    var b = 2;
+    // The above variable is var b
+    print a + b;
+    """
+
+    # WHEN
+    stdout, stderr = lox_runner(code)
+
+    # THEN
+    assert stdout.strip().splitlines() == ["17"]
+    assert stderr == ""
